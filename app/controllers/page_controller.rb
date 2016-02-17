25746052f82a60662048f7c4c83651b17081889d
tan lim <ckeenlim@gmail.com>
2016-02-17 13:39:54 -0500

    all files

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,19 @@
+class PageController < ApplicationController
+  def home
+  end
+
+  def vendors
+  end
+
+  def about
+  end
+
+  def faqs
+  end
+
+  def contact
+  end
+
+  def checklist
+  end
+end
