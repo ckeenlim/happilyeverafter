0c448ddbe703c0bee2f5bc9fb05ae94c7258d078
tan lim <ckeenlim@gmail.com>
2016-03-05 11:22:22 -0500

    merge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,11 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
+
 //= require_tree .
 
 //= require bootstrap-sprockets
+//= require jquery_ujs
+
+//= require turbolinks
 
