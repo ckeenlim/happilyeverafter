3f8eb06f6204a2d54b3bac7f68cb26ae4f8755f3
tan lim <ckeenlim@gmail.com>
2016-04-04 01:07:03 -0400

    fix require_tree in js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require bootstrap-sprockets
-//= require_tree .
 //= require turbolinks
 //= require bootstrap-slider.min
 //= require hover-dropdown
